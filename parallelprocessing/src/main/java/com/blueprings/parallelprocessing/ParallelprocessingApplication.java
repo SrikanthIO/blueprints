7e5dd3886619548c38028a344c24d79c15798c0f
finvedicdev <128971579+finvedicdev@users.noreply.github.com>
2024-05-03 10:30:24 +0530

    Init Checkin

---
+++ parallelprocessing/src/main/java/com/blueprings/parallelprocessing/ParallelprocessingApplication.java
@@ -0,0 +1,13 @@
+package com.blueprings.parallelprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParallelprocessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParallelprocessingApplication.class, args);
+	}
+
+}
---
+++ parallelprocessing/src/test/java/com/blueprings/parallelprocessing/ParallelprocessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blueprings.parallelprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParallelprocessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
